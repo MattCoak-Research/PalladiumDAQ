92f0cc968fc7948d57d9907032e774617990ecae
Matt Coak <m.j.coak@bham.ac.uk>
2025-12-06 15:26:22 +0000

    Added a PPMS instrument and accompanying Instrument Drivers. This assumes a PPMS is running the QDInstrument Server programme on a separate host computer that the CoakView PC is networked too. A C# .NET wrapper dll is needed to wrap the dll from Quantum Design provided for this purpose, as theirs simply does not work with MATLAB, despite the promises of its documentation (type mismatches with the enums). Source files for building that interface dll included

---
+++ CoakView/+CoakViewPresets/PPMS.m
@@ -0,0 +1,18 @@
+function PPMS(coakView)
+%PPMS - COakVIew preset function for when interfacing with a remote computer controlling a PPMS cryostat.
+
+
+%Add the PPMS and configure
+ppms = coakView.AddInstrument("PPMS");
+ppms.IP_Address = "169.254.152.146";
+ppms.Name = "PPMS";
+
+%Add a Plotting tab - this returns a list of all the plotters in it
+pltrs = coakView.AddNewPlottingTab(2, 1);
+pltrs(1).SetDefaultXAxis("Time (mins)");
+pltrs(2).SetDefaultXAxis("Time (mins)");
+pltrs(1).SetDefaultYAxes("PPMS - Temperature_K", [], [], []);
+pltrs(2).SetDefaultYAxes("PPMS - Field_T", [], [], []);
+
+end
+
