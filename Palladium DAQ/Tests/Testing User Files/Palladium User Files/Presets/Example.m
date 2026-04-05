908891a26a9641e4bc3ea8b4a8d6a7ef27904d4a
MattThePie <matthew.coak@gmail.com>
2026-04-05 19:01:39 +0100

    Added pre-made User Files for the Test Config. A separate test should run to check that these files get created ok if not present, but to make sure tests always run with the same initial conditions, this should be present on test pc from first run

---
+++ Palladium DAQ/Tests/Testing User Files/Palladium User Files/Presets/Example.m
@@ -0,0 +1,38 @@
+function Example(palladium, gui)
+%EXAMPLE - Example/template preset for Palladium. Edit it, save as
+%'Filename' in the Presets folder, then use it by calling
+%Palladium('Filename') to programmatically set various parameters and GUI
+%preferences to suit a particular setup or experiment. All of this can be
+%done in the GUI, this is solely for convenience.
+
+
+%Add an instrument of specified type
+palladium.AddInstrument("Keithley2000");
+
+
+%Add an instrument of specified type then modify its properties
+instr = palladium.AddInstrument("Lakeshore350");
+instr.Connection_Type = Palladium.Enums.ConnectionType.Debug;
+instr.Ch_A_Name = "1 K Pot Temp (K)";
+instr.Ch_C_Name = "Sample Stage Temp (K)";
+
+
+%Add an instrument of specified type  then modify its properties - adding a
+%sweep control panel to it to show off
+k24 = palladium.AddInstrument("Keithley2410");
+k24.SweepControl = true;
+k24.MeasMode = k24.MeasType("Current"); %Example of how to set Categorical properties in Preset files
+
+
+%Add a Plotting tab
+pltr = gui.AddNewPlottingTab(1, 1);
+pltr.SetDefaultXAxis("Time (mins)");
+pltr.SetDefaultYAxes("Time (mins)", [], "K2000_1 - Resistance (Ohms)", []);
+gui.AddNewPlottingTab(2, 2);
+
+
+%Add a Plotting window
+%gui.AddNewPlottingWindow(1, 1);
+
+end
+
