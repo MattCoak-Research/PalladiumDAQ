6ffaaf3312196ad05b19bb475d4ad281924b88b7
MattCoak-Research <m.j.coak@bham.ac.uk>
2026-05-01 16:21:37 +0100

    Added some checks and comments to Python Utils class

--- Palladium DAQ/+Palladium/+Utilities/PythonUtils.m
+++ Palladium DAQ/+Palladium/+Utilities/PythonUtils.m
@@ -6,6 +6,21 @@ classdef PythonUtils
     methods (Static, Access = public)
 
         function AppendFolderToPythonPath(directoryPath)
+            %APPENDFOLDERTOPYTHONPATH - Add a directory to MATLAB's Python path
+            %
+            % Input arguments:
+            % directoryPath - folder path to prepend to Python search paths
+            %
+            % This function ensures the provided directory is available to the
+            % Python interpreter invoked from MATLAB.
+            arguments
+                directoryPath {mustBeTextScalar};
+            end
+
+            %Check the path exists first
+            assert(exist(directoryPath, 'dir')==7, "PythonPathError:NoSuchDirectory", "Directory " + strrep(directoryPath, "\", "\\") + " not found, could not add to Python path in PythonUtils");
+           
+            %Add to python search path inside MATLAB
             pyrun("import sys");
             pyrun("sys.path.append(r""" + directoryPath + """)");
         end
